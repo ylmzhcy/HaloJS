623b94af79c8a184cf01f27f713f1dd229287a66
Halil Can Yılmaz <44755095+ylmzhcy@users.noreply.github.com>
2021-08-08 21:15:33 +0300

    Calculator

---
+++ 5- Basic Calculator with since/index.js
@@ -0,0 +1,34 @@
+let num1 = 8;
+let num2 = 2;
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+
+let str = document.getElementById("sum-el").innerText;
+// Create four functions: add(), subtract(), divide(), multiply()
+function add() {
+  let result = num1 + num2;
+  document.getElementById("sum-el").innerText =
+    "Sum :" + " " + result + " || " + "(since : " + num1 + " + " + num2 + ")";
+}
+function subtract() {
+  let result = num1 - num2;
+  document.getElementById("sum-el").innerText =
+    "Sum :" + " " + result + " || " + "(since : " + num1 + " - " + num2 + ")";
+}
+function divide() {
+  let result = num1 / num2;
+  document.getElementById("sum-el").innerText =
+    "Sum :" + " " + result + " || " + "(since : " + num1 + " / " + num2 + ")";
+}
+function multiply() {
+  let result = num1 * num2;
+  document.getElementById("sum-el").innerText =
+    "Sum :" + " " + result + " || " + "(since : " + num1 + " * " + num2 + ")";
+}
+// Call the correct function when the user clicks on one of the buttons
+//ok
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
