a41cf61a4d2027b55f50fd4d8ef683755071f421
Halil Can Yılmaz <44755095+ylmzhcy@users.noreply.github.com>
2021-10-02 17:28:35 +0300

    odev3

---
+++ PatikaDev/Odev3/circle.js
@@ -0,0 +1,7 @@
+module.exports.circleArea = () => {
+  console.log();
+};
+
+module.exports.circleCircumference = () => {
+  console.log("Yarı Çap : 5");
+};
---
+++ PatikaDev/Odev3/index.js
@@ -0,0 +1,3 @@
+const circle = require("./circle");
+
+circle.circleCircumference();
